a8f1b3c392d256c03792eed5abeb187cf41d1fe5
carlo emilio montanari <carlidel95@gmail.com>
2021-02-23 18:58:54 +0100

    necessary correction for dumping output to eos

--- scripts/henon_4d_general_run.sh
+++ scripts/henon_4d_general_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export EOS_MGM_URL=root://eosuser.cern.ch
 export MYPYTHON=/afs/cern.ch/work/c/camontan/public/miniconda3
 
 source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev4cuda9/latest/x86_64-centos7-gcc62-opt/setup.sh
@@ -16,4 +17,6 @@ mkdir img
 echo $1
 echo $2
 
-python3 henon_4d_general_run.py basic $1 $2 500 polar 10000000 1e-10 1e-11 10 100000 49 100 1e-12 subbasic 0 1 0 1 0 0 -outdir /afs/cern.ch/work/c/camontan/public/dynamic_indicator_analysis/data
+python3 henon_4d_general_run.py basic $1 $2 500 polar 10000000 1e-10 1e-11 10 100000 49 100 1e-12 subbasic 0 1 0 1 0 0 -outdir ./data
+
+eos cp data/* /eos/project/d/da-and-diffusion-studies/DA_Studies/Simulations/Models/dynamic_indicator_analysis/dynamic_indicator_analysis/data
