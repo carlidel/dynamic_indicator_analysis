4279f98fb478cffb6c1bb8f1f221f5dd7a39c9ac
carlo emilio montanari <carlidel95@gmail.com>
2021-02-26 12:03:43 +0100

    little regex mistake

--- scripts/gather_reversibility_to_REI.py
+++ scripts/gather_reversibility_to_REI.py
@@ -17,5 +17,5 @@ file_list = open("input_reversibility_to_REI.txt", 'w')
 for i, f in enumerate(data_file_list):
     if "inverse_tracking" in f:
         source_name = re.sub('orto_displacement', 'init', f)
-        source_name = re.sub(r"_subid_.*\.", r"\.", source_name)
+        source_name = re.sub(r"_subid_.*\.", ".", source_name)
         file_list.write(data_dir + " " + source_name + " " + f + "\n")
